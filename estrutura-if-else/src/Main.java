703d37b23a77d6b2a2d466a55166ac2e8b0dcb2a
GabrielAlb1998 <gabrielalbuquerque640@gmail.com>
2025-07-31 09:09:35 -0300

    atualização

---
+++ estrutura-if-else/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main{
+
+    public static void main (String[] args){
+        var scanner = new Scanner(System.in);
+        System.out.println("Qual seu nome?");
+        var name = scanner.next();
+        System.out.println("Qual sua idade?");
+        var age = scanner.nextInt();
+
+        if(age >= 18) System.out.printf("Olá %s, você é de maior!", name);
+        else System.out.printf("Olá %s, você é de menor!", name);
+
+
+    }
+
+}
